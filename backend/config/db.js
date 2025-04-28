f6715aa2b0f4e51449671c3dc605a277cf928bad
Fayre000 <faycalkordjani@gmail.com>
2025-04-28 13:20:23 +0200

    add files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,13 @@
-// backend/config/db.js
-
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Erreur de connexion à MySQL :', err.message);
-    } else {
-        console.log('Connexion réussie à MySQL.');
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true
     }
 });
 
